69a8eae7e5cc37237447bb4d174340a53bce69c5
dongha922 <kdh4575@naver.com>
2023-12-15 01:33:58 +0900

    ìwork

---
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.ll.medium.domain.member.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    public void join(String username, String password){
+
+    }
+}
